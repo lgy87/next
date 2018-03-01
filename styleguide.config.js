ac447401d59a2cf444c488004e09c44626461c64
Guangyao Li <lgy87@foxmail.com>
2018-03-01 15:20:25 +0800

    [feature] styleguide add url-loader.

--- styleguide.config.js
+++ styleguide.config.js
@@ -40,6 +40,18 @@ module.exports = {
             },
           ],
         },
+        {
+          test: /\.(jpe?g|svg|png|gif)$/,
+          use: [
+            {
+              loader: 'url-loader',
+              options: {
+                limit: 100000,
+                name: 'images/[hash:20].[ext]',
+              },
+            },
+          ],
+        },
       ],
     },
     context: SRC,
