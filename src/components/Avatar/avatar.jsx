9ec9deef1294a8ae1fed6ba381a4c711dbf8aa98
Guangyao Li <lgy87@foxmail.com>
2018-03-01 15:37:10 +0800

    [feature] add [Avatar] component.

---
+++ src/components/Avatar/avatar.jsx
@@ -0,0 +1,110 @@
+import {addPropTypesTo_} from "utils/addTo_"
+import createPropTypesBy from "utils/createPropTypesBy"
+import removePropsIf from "HOCs/removePropsIf"
+import mapPropsIf from "HOCs/mapPropsIf"
+import attachClassName from "HOCs/attachClassName"
+import patchClassName from "HOCs/patchClassName"
+import findInBy from "utils/findInBy"
+
+const status = ["online", "busy", "away", "offline"]
+const Presence = props => <i {...props} />
+
+const AvataPresence = r.pipe(
+  addPropTypesTo_({
+    ...createPropTypesBy(
+      r.always("bool"),
+      status,
+    ),
+    className: "string",
+    style: "object",
+  }),  
+  removePropsIf({
+    list: status,
+    pred: r.T,
+  }),
+  attachClassName("avatar-presence"),
+  patchClassName({
+    list: status,
+  }),
+)(Presence)
+
+const size = ["xs", "sm", "lg", "xl"]
+const appearance = ["badge"]
+const baseClassName = "avatar"
+
+function Avatar ({
+  src,
+  alt,
+  iconSrc,
+  iconAlt,
+  online,
+  busy,
+  away,
+  offline,
+  ...rest
+}) {
+  const statusObj = {online, busy, away, offline}
+  const avatarPresenceVisible = findInBy(r.equals(true), status, statusObj).length > 0
+
+  return (
+    <figure {...rest}>
+      {
+        src && <img {...{src}} {...{alt}} />
+      }
+      {
+        iconSrc && <img src={iconSrc} className="avatar-icon" alt={iconAlt} />
+      }
+      {
+        avatarPresenceVisible && <AvataPresence {...statusObj} /> 
+      }
+    </figure>
+  )
+}
+
+export default r.pipe(
+  addPropTypesTo_({
+    ...createPropTypesBy(
+      r.always("bool"),
+      size.concat(appearance, status),
+    ),
+    src: "string",
+    alt: "string",
+    iconSrc: "string",
+    iconAlt: "string",
+    badge: ["oneOfType", "string", "number", "bool"],
+    badgeValue: ["oneOfType", "string", "number"],
+    text: "string",
+    children: "node",
+    className: "string",
+    style: "object",
+  }),  
+  removePropsIf({
+    list: size.concat(appearance),
+  }),
+  attachClassName(baseClassName),
+  patchClassName({
+    list: size,
+    transformer: r.concat(`${baseClassName}-`),
+  }),
+  patchClassName({
+    list: appearance,
+  }),
+  mapPropsIf({
+    list: ["text"],
+    pred: r.T,
+    transformer: value => ({"data-initial": value})
+  }),
+  mapPropsIf({
+    list: ["badgeValue"],
+    pred: r.T,
+    transformer: value => ({"data-badge": value})
+  }),
+  mapPropsIf({
+    list: ["badge"],
+    pred: r.anyPass([r.is(Number), r.is(String)]),
+    transformer: value => ({
+      badge: true,
+      "data-badge": value,
+    })
+  }),
+)(Avatar)
---
+++ src/components/Avatar/readme.md
@@ -0,0 +1,55 @@
+```js
+<>
+  <Avatar xl src="https://avatars3.githubusercontent.com/u/17808424?s=460&v=4" />
+  <Avatar lg src="https://avatars3.githubusercontent.com/u/17808424?s=460&v=4" />
+  <Avatar    src="https://avatars3.githubusercontent.com/u/17808424?s=460&v=4" />
+  <Avatar sm src="https://avatars3.githubusercontent.com/u/17808424?s=460&v=4" />
+  <Avatar xs src="https://avatars3.githubusercontent.com/u/17808424?s=460&v=4" />
+</>
+```
+```js
+<>
+  <Avatar xl text="LG" />
+  <Avatar lg text="LG" />
+  <Avatar    text="LG" />
+  <Avatar sm text="LG" />
+  <Avatar xs text="LG" />
+</>
+```
+```js
+<>
+  <Avatar
+    xl
+    text="LG"
+    src="https://avatars3.githubusercontent.com/u/17808424?s=460&v=4"
+    iconSrc="https://avatars3.githubusercontent.com/u/17808424?s=460&v=4"
+  />
+  <Avatar
+    lg
+    text="LG"
+    iconSrc="https://avatars3.githubusercontent.com/u/17808424?s=460&v=4"
+    iconAlt="iconAlt"
+  />
+</>
+```
+```js
+<>
+  <Avatar
+    xl
+    src="https://avatars3.githubusercontent.com/u/17808424?s=460&v=4"
+    online
+  />
+  <Avatar
+    lg
+    text="LG"
+    busy
+    iconAlt="iconAlt"
+  />
+</>
+```
+
+<style>
+  .avatar {
+    margin-right: 5px;
+  }
+</style>
